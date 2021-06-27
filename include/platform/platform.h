6158fa69bde9b58a6af6a39432519524b185d49d
Alexander Orlov <me@0xceed.com>
2021-06-27 22:27:21 +0300

    Reformat.

--- include/platform/platform.h
+++ include/platform/platform.h
@@ -2,8 +2,8 @@
 #define PLATFORM_H
 
 /// Provides pointer to the first of 'length' bytes from 'input'.
-typedef void (yc_res_platform_reader_t)(const void* input,
-                                        unsigned long offset, unsigned long length,
-                                        unsigned char* output);
+typedef void yc_res_platform_reader_t(const void* input,
+                                      unsigned long offset, unsigned long length,
+                                      unsigned char* output);
 
 #endif /* PLATFORM_H */
--- sources/formats/dat/dat.c
+++ sources/formats/dat/dat.c
@@ -8,9 +8,6 @@ void yc_res_dat_count(yc_res_platform_reader_t* reader, const void* input, unsig
     const count = 4;
     unsigned char slice[count];
     
-    // read first 4 bytes
     reader(input, 0, count, &slice[0]);
-    
-    // assemble 32-bit unsigned integer in big-endian
     *output = (slice[0] << 24) + (slice[1] << 16) + (slice[2] << 8) + slice[3];
 }

28bbb29eda267d3196f66410d24e1add4e564b9f
Alexander Orlov <me@0xceed.com>
2021-06-28 16:45:45 +0300

    Rearrange headers.

--- sources/formats/dat/dat.c
+++ sources/formats/dat/dat.c
@@ -1,14 +1,9 @@
 #include "formats/dat/dat.h"
+#include "dat.h"
 
 #include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
-
-void yc_res_dat_private_load_uint(yc_res_platform_reader_t* reader, const void* input,
-                                  unsigned int offset, unsigned int* count);
-
-void yc_res_dat_private_load_string(yc_res_platform_reader_t* reader, const void* input,
-                                    unsigned int offset, char** value, unsigned int* read);
 
 void yc_res_dat_count(yc_res_platform_reader_t* reader, const void* input, unsigned int* count) {
     if (NULL == reader || NULL == input || NULL == count)
@@ -93,32 +88,4 @@ void yc_res_dat_file_free(yc_res_dat_file_t* file) {
         free(file->name);
         file->name = NULL;
     }
-}
-
-// MARK: - Private
-
-void yc_res_dat_private_load_uint(yc_res_platform_reader_t* reader, const void* input,
-                                  unsigned int offset, unsigned int* value) {
-    const length = 4;
-    unsigned char slice[length];
-    
-    reader(input, offset, length, &slice[0]);
-    *value = (slice[0] << 24) + (slice[1] << 16) + (slice[2] << 8) + slice[3];
-}
-
-void yc_res_dat_private_load_string(yc_res_platform_reader_t* reader, const void* input,
-                                    unsigned int offset, char** value, unsigned int* read) {
-    unsigned char length = 0;
-    
-    reader(input, offset, 1, &length);
-    offset++;
-    
-    *read = length + 1;
-    *value = malloc(length + 1);
-    
-    if (NULL == *value)
-        return; // TODO: Handle errors.
-    
-    reader(input, offset, length, (unsigned char*)*value);
-    (*value)[length] = '\0';
 }

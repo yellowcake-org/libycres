255dc60b5b8f103e1763ad3b63a77fc7d99db5df
Alexander Orlov <me@0xceed.com>
2021-07-01 01:07:49 +0300

    Added CLI to example.

--- examples/undat/include/print/print.h
+++ examples/undat/include/print/print.h
@@ -4,5 +4,6 @@
 #include <undat.h>
 
 void undat_print_node(yc_res_dat_directory_t* node, unsigned long level);
+void undat_print_arg_errors(struct arg_end* end, char* name);
 
 #endif /* UNDAT_PRINT_H */
--- examples/undat/sources/main.c
+++ examples/undat/sources/main.c
@@ -4,20 +4,70 @@
 #include <stdlib.h>
 
 int main(__unused int argc, char *argv[]) {
-    FILE* handle = fopen(argv[1], "rb");
+    struct arg_lit *help, *version, *list;
+    struct arg_file *input;
+    struct arg_end *end;
         
-    yc_res_dat_directory_t *root;
-    yc_res_dat_tree(&undat_platform_file_reader, handle, &root);
+    void *(argtable[5]);
+    char progname[] = "undat";
     
-    undat_iterate_tree(root, 0, &undat_print_node);
+    int nerrors;
+    int result = 1;
     
-    yc_res_dat_free_tree(root);
+    input = arg_filen(NULL, "input", "<file>", 1, 1, "path to archive file");
+    list = arg_litn(NULL, "list", 0, 1, "list contents");
+    help = arg_litn(NULL, "help", 0, 1, "display this help and exit");
+    version = arg_litn(NULL, "version", 0, 1, "display version info and exit");
+    end = arg_end(2);
     
-    free(root);
-    root = NULL;
+    argtable[0] = input;
+    argtable[1] = list;
+    argtable[2] = help;
+    argtable[3] = version;
+    argtable[4] = end;
     
-    fclose(handle);
-    handle = NULL;
+    nerrors = arg_parse(argc,argv,argtable);
+
+    if (help->count > 0) {
+        printf("Usage: %s", progname);
+        arg_print_syntaxv(stdout, argtable, "\n");
+        
+        printf("Utility for working with Fallout™ resource archives.\n\n");
+        arg_print_glossary(stdout, argtable, "  %-25s %s\n");
+                
+        arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+        return 0;
+    }
+
+    if (nerrors > 0) {
+        undat_print_arg_errors(end, progname);
+    } else {
+        FILE* file = fopen(input->filename[0], "rb");
+        
+        if (NULL != file) {
+            if (list->count > 0) {
+                yc_res_dat_directory_t *root;
+                yc_res_dat_tree(&undat_platform_file_reader, file, &root);
+                
+                undat_iterate_tree(root, 0, &undat_print_node);
+                
+                yc_res_dat_free_tree(root);
+                
+                free(root);
+                root = NULL;
+                
+                result = 0;
+            } else {
+                undat_print_arg_errors(end, progname);
+            }
+        }
+        
+        if (NULL != file) {
+            fclose(file);
+            file = NULL;
+        }
+    }
     
-    return 0;
+    arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+    return result;
 }

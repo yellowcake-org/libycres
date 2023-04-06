468c2145b0bd216e95728a8901ad6f53a4f2340f
Alexander Orlov <me@0xceed.com>
2023-04-06 21:52:23 +0400

    Drafted CLI for ycundat.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- examples/ycundat/include/ycundat.h
+++ examples/ycundat/include/ycundat.h
@@ -1,7 +1,9 @@
 #ifndef YCUNDAT_H
 #define YCUNDAT_H
 
+#include <argtable3.h>
 #include <libycres.h>
+
 #include "main.h"
 
 #endif //YCUNDAT_H
--- examples/ycundat/source/main.c
+++ examples/ycundat/source/main.c
@@ -1,5 +1,43 @@
 #include <ycundat.h>
 
-int main(void) {
-    yc_res_dat_hello();
+/* global arg_xxx structs */
+struct arg_lit *help;
+struct arg_file *input, *output;
+struct arg_end *end;
+
+int main(int argc, char *argv[]) {
+    void *arg_table[] = {
+            help = arg_litn(NULL, "help", 0, 1, "display this help and exit"),
+            input = arg_filen("i", "input", "<file.dat>", 1, 1, "input file"),
+            output = arg_filen("o", "output", "<directory>", 1, 1, "output directory"),
+            end = arg_end(2),
+    };
+
+    int exit_code = 0;
+    char program_name[] = "ycundat";
+
+    int errors_count;
+    errors_count = arg_parse(argc, argv, arg_table);
+
+    if (help->count > 0) {
+        printf("Usage: %s", program_name);
+        arg_print_syntax(stdout, arg_table, "\n");
+        arg_print_glossary(stdout, arg_table, "  %-25s %s\n");
+        exit_code = 0;
+
+        goto exit;
+    }
+
+    if (errors_count > 0) {
+        arg_print_errors(stdout, end, program_name);
+        printf("Try '%s --help' for more information.\n", program_name);
+        exit_code = 1;
+
+        goto exit;
+    }
+
+    exit:
+    arg_freetable(arg_table, sizeof(arg_table) / sizeof(arg_table[0]));
+
+    return exit_code;
 }

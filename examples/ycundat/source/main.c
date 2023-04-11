b06ceffecaff4d4cfa3a616cb14a8604cf66479d
Alexander Orlov <me@0xceed.com>
2023-04-12 00:54:58 +0300

    Drafted reading directories.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- examples/ycundat/source/main.c
+++ examples/ycundat/source/main.c
@@ -1,9 +1,14 @@
 #include <ycundat.h>
 
+#include <memory.h>
+#include <stdlib.h>
+
 /* global arg_xxx structs */
 struct arg_lit *help;
 struct arg_file *input, *output;
 struct arg_end *end;
+
+int list(const char *filename);
 
 int main(int argc, char *argv[]) {
     void *arg_table[] = {
@@ -36,8 +41,57 @@ int main(int argc, char *argv[]) {
         goto exit;
     }
 
+    if (input->count == 1) {
+        exit_code = list(input->filename[0]);
+    }
+
     exit:
     arg_freetable(arg_table, sizeof(arg_table) / sizeof(arg_table[0]));
 
     return exit_code;
+}
+
+int list(const char *filename) {
+    FILE *archive = fopen(filename, "rb");
+    if (NULL == archive) { return 2; }
+
+    unsigned char dir_count_bytes[4];
+    fread(&dir_count_bytes, 4, 1, archive);
+
+    // TODO: Move out BE/LE conversions.
+    u_int32_t dir_count = 0;
+    memcpy((char *) &dir_count + 0, &dir_count_bytes[3], 1);
+    memcpy((char *) &dir_count + 1, &dir_count_bytes[2], 1);
+    memcpy((char *) &dir_count + 2, &dir_count_bytes[1], 1);
+    memcpy((char *) &dir_count + 3, &dir_count_bytes[0], 1);
+
+    printf("Directories: %zu\n", (size_t) dir_count);
+
+    fseek(archive, 4 * 3, SEEK_CUR);
+
+    // TODO: Create appropriate structures.
+    for (u_int32_t dir_num = 0; dir_num < dir_count; ++dir_num) {
+        char length = 0;
+        fread(&length, 1, 1, archive);
+
+        char *path = malloc((size_t) length + 1);
+        if (NULL == path) {
+            fclose(archive);
+            return 3;
+        }
+
+        fread(path, length, 1, archive);
+        path[(size_t) length] = 0;
+
+        printf("%s\n", path);
+        free(path);
+    }
+
+    // TODO: Iterate over files' entries.
+    for (u_int32_t dir_num = 0; dir_num < dir_count; ++dir_num) {
+        //
+    }
+
+    fclose(archive);
+    return 0;
 }

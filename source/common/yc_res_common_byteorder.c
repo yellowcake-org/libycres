fbbfd54772c03b52b307ee5706e35a7b236b0f34
Alexander Orlov <me@0xceed.com>
2023-05-02 00:22:37 +0300

    Fix typos.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- source/common/yc_res_common_byteorder.c
+++ source/common/yc_res_common_byteorder.c
@@ -13,7 +13,7 @@ uint16_t yc_res_byteorder_uint16(uint16_t input) {
     return (input >> 8) | (input << 8);
 }
 
-uint32_t yc_res_byteorder_uint32(u_int32_t input) {
+uint32_t yc_res_byteorder_uint32(uint32_t input) {
     return ((input >> 24) & 0xff) |
            ((input << 8) & 0xff0000) |
            ((input >> 8) & 0xff00) |

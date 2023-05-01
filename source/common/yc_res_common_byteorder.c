959b81c7424bbbe0383944139e4a146d6e7ce21b
Alexander Orlov <me@0xceed.com>
2023-05-02 00:12:55 +0300

    Fix UB.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- source/common/yc_res_common_byteorder.c
+++ source/common/yc_res_common_byteorder.c
@@ -1,12 +1,12 @@
 #include <private.h>
 
 int16_t yc_res_byteorder_int16(int16_t input) {
-    return ((((input) >> 8) & 0xff) | (((input) & 0xff) << 8));
+    return (int16_t) (((((int32_t) input) >> 8) & 0xff) | ((((int32_t) input) & 0xff) << 8));
 }
 
 int32_t yc_res_byteorder_int32(int32_t input) {
-    input = (input << 8 & 0xFF00FF00) | (input >> 8 & 0xFF00FF);
-    return (input << 16) | ((input >> 16) & 0xFFFF);
+    int64_t result = (input << 8 & 0xFF00FF00) | (input >> 8 & 0xFF00FF);
+    return (int32_t) ((result << 16) | ((result >> 16) & 0xFFFF));
 }
 
 uint16_t yc_res_byteorder_uint16(uint16_t input) {

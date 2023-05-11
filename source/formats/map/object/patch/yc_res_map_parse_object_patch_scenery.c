85ff816fe8e011bcb773be32202cead3c06f1dd0
Alexander Orlov <me@0xceed.com>
2023-05-11 15:36:35 +0300

    Rename short params name.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- source/formats/map/object/patch/yc_res_map_parse_object_patch_scenery.c
+++ source/formats/map/object/patch/yc_res_map_parse_object_patch_scenery.c
@@ -9,14 +9,14 @@
 yc_res_map_status_t yc_res_map_parse_object_patch_scenery(
         void *file,
         const yc_res_io_fs_api_t *api,
-        const yc_res_map_parse_db_api_t *db,
+        const yc_res_map_parse_db_api_t *fetchers,
         uint32_t pid,
         yc_res_map_level_object_patch_t *into
 ) {
     into->scenery = malloc(sizeof(yc_res_map_level_object_patch_scenery_t));
     if (NULL == into->scenery) { return YC_RES_MAP_STATUS_MEM; }
 
-    into->scenery->type = db->scenery_type_from_pid(pid, db->context);
+    into->scenery->type = fetchers->scenery_type_from_pid(pid, fetchers->context);
     switch (into->scenery->type) {
         case YC_RES_PRO_OBJECT_SCENERY_TYPE_DOOR:
             into->scenery->data.door = malloc(sizeof(yc_res_map_level_object_patch_scenery_door_t));

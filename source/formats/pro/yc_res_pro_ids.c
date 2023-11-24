07be155f0ae8730dda1a907bd47f87753b66f401
Alexander Orlov <me@0xceed.com>
2023-11-24 04:58:01 +0300

    Convenient FID methods.
    Signed-off-by: Alexander Orlov <me@0xceed.com>

--- source/formats/pro/yc_res_pro_ids.c
+++ source/formats/pro/yc_res_pro_ids.c
@@ -11,3 +11,7 @@ uint16_t yc_res_pro_index_from_object_id(uint32_t any_id) {
 uint16_t yc_res_pro_index_from_script_id(uint32_t script_id) {
     return script_id & 0x00FFFFFF;
 }
+
+uint16_t yc_res_pro_index_from_sprite_id(uint32_t sprite_id) {
+    return sprite_id & 0x00FFFFFF;
+}
